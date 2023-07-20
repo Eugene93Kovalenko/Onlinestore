44e1e52b7e4ee2babfb96221e359703385c61e63
Eugene <keugenemail@gmail.com>
2023-07-20 12:01:05 +0300

    Перешел на postgresql

---
+++ onlinestore_project/product_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=20)
+    money = models.CharField(max_length=20, default='asd')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Цвет"
+        verbose_name_plural = "Цвета"
