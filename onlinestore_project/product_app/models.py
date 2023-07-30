3567cf61b20cc05894dad33d20f0ecccfac8aaf4
Eugene <keugenemail@gmail.com>
2023-07-30 22:35:01 +0300

    Добавил в модели: категорию, продукт, цвет, размер

--- onlinestore_project/product_app/models.py
+++ onlinestore_project/product_app/models.py
@@ -1,9 +1,24 @@
 from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(blank=True, upload_to="category/%Y/%m/%d/")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("category", kwargs={"category_name": self.name.lower()})
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
 
 class Color(models.Model):
     name = models.CharField(max_length=20)
-    money = models.CharField(max_length=20, default='asd')
 
     def __str__(self):
         return self.name
@@ -13,4 +28,78 @@ class Color(models.Model):
         verbose_name_plural = "Цвета"
 
 
-text = 'asd'
+class Size(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Размер"
+        verbose_name_plural = "Размеры"
+
+
+class Product(models.Model):
+    class ColorChoices(models.TextChoices):
+        Black = "Black"
+        Red = "Red"
+        White = "White"
+        Yellow = "Yellow"
+        Blue = "Blue"
+        Green = "Green"
+
+    COLLECTION_CHOICE = [
+        ("winter", "Winter"),
+        ("spring", "Spring"),
+        ("summer", "Summer"),
+        ("autumn", "Autumn"),
+    ]
+
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE, related_name="products")
+    color = models.ForeignKey(Color, on_delete=models.CASCADE, related_name="products")
+    color_choices = models.CharField(
+        max_length=10, choices=ColorChoices.choices, blank=True
+    )
+    description = models.TextField(blank=False)
+    information = models.TextField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    num_visits = models.IntegerField(default=0)
+    collection = models.CharField(max_length=10, choices=COLLECTION_CHOICE, blank=True)
+    barcode_number = models.IntegerField(blank=False)
+    category = models.ManyToManyField(Category, related_name="products")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("product", kwargs={"product_id": self.pk})
+
+    def get_collection_absolute_url(self):
+        return reverse("collection", kwargs={"collection_name": self.collection})
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+        ordering = ["name"]
+
+
+class ProductImage(models.Model):
+    image = models.ImageField(upload_to="product/%Y/%m/%d/")
+    is_main = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="images"
+    )
+
+    class Meta:
+        verbose_name = "Фото"
+        verbose_name_plural = "Фото"
+
+    def __str__(self):
+        return self.image.name
+
+    def get_absolute_url(self):
+        return self.image.url
--- onlinestore_project/product_app/views.py
+++ onlinestore_project/product_app/views.py
@@ -1,5 +1,2 @@
 from django.shortcuts import render
 
-
-def index():
-    pass
